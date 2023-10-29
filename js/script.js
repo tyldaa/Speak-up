452ceafb54dd1367e225fa08f30a0bfccc378447
tyldaa <wojcicka_kinga@wp.pl>
2023-10-29 16:52:19 +0100

    delete js content

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-const fetchPokemon = async () => {
-  console.log("start");
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/35");
-  console.log("end");
-  console.log(response);
-  if (response.ok) {
-    const pokemon = await response.json();
-    console.log(pokemon);
-  }
-};
-fetchPokemon();
+
